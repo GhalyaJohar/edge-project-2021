d1fef057d470554e989970487709038338708037
GhalyaJohar <87119348+GhalyaJohar@users.noreply.github.com>
2021-07-12 22:36:30 +0300

    Create seats available.py

---
+++ seats available.py
@@ -0,0 +1,28 @@
+# print("python project")
+# #Day 1
+# s#Hello, Welcome to S&G's cenima! Type Hey to see the available services!
+# s# 1-List of movies + Age requirement
+# s# 2-Ehtraz if vaccinated y if not vaccinated print "sorry you cannot access the cenima"
+# s# 3-List of movie showtime (each movie printed with it's showtime)
+# g# 4-if age >13 not allowed without parent quardian if age 18=< print please choose seat
+# s# 5-list = available seats, row a n/ row n/
+# s# 6-print the seat chosen (seat A3) (it will be deleted from the og list)
+# g# 7-available food (butter popcorn caramel popcorn, cheese popcorn, nachos, hotdog, fries, slush, water, coca cola, sprite )
+# g# 8-do you want the food delivered to your seat or pickup.
+# g# Thank you, enjoy your movie
+#
+# Movie names: The purge | Cruella | The Conjouring
+# Seat list: A1-A5 | B1-B5 | C1-C5 | D1-D5
+
+# command = input("Hey, how old are you ? ")
+# while command!= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17] :
+#     if command == [18,19]:
+#         print("which movie")
+#         command= input("thats a great choice")
+# print("sorry, you cant choose this movie")
+
+Seats-Room1 = ["A1","A2","A3","A4","A5","B1","B2","B3","B4","B5","C1","C2","C3","C4","C5","D1","D2","D3","D4","D5"]
+Seats-Room2 = ["A1","A2","A3","A4","A5","B1","B2","B3","B4","B5","C1","C2","C3","C4","C5","D1","D2","D3","D4","D5"]
+Seats-Room3 = ["A1","A2","A3","A4","A5","B1","B2","B3","B4","B5","C1","C2","C3","C4","C5","D1","D2","D3","D4","D5"]
+
+
