222e5da2a89c79349457e37b695209e269bab30b
Sara <saro0ona_2003@hotmail.com>
2021-07-12 11:32:43 +0300

    Update python project .py
    Project list

--- python project .py
+++ python project .py
@@ -1,2 +1,10 @@
 print("python project")
 #Day 1
+#1-List of movies + Age requirement
+#2-Ehtraz if vaccinated y if not vaccinated print "sorry you cannot access the cenima"
+#3-List of movie showtime (each movie printed with it's showtime)
+#4-if age >13 not allowed without parent     quardian if age 18=< print please choose seat
+#5-list = available seats, row a n/ row n/ 
+#6-print the seat chosen (seat A3) (it will be deleted from the og list)
+#7-available food (butter popcorn caramel popcorn, cheese popcorn, nachos, hotdog, fries, slush, water, coca cola, sprite )
+#8-do you want the food delivered to your seat or pickup.
