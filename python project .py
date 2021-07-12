ecc001101ca4da7e6e2077db7ed7c75bf5265747
Sara <saro0ona_2003@hotmail.com>
2021-07-12 11:53:02 +0300

    Update python project .py
    FINAL PROJECT

--- python project .py
+++ python project .py
@@ -1,11 +1,15 @@
 print("python project")
 #Day 1
-#1-List of movies + Age requirement
-#2-Ehtraz if vaccinated y if not vaccinated print "sorry you cannot access the cenima"
-#3-List of movie showtime (each movie printed with it's showtime)
-#4-if age >13 not allowed without parent     quardian if age 18=< print please choose seat
-#5-list = available seats, row a n/ row n/ 
-#6-print the seat chosen (seat A3) (it will be deleted from the og list)
-#7-available food (butter popcorn caramel popcorn, cheese popcorn, nachos, hotdog, fries, slush, water, coca cola, sprite )
-#8-do you want the food delivered to your seat or pickup.
-#Thank you, enjoy your movie 
+s#Hello, Welcome to S&G's cenima! Type Hey to see the available services!
+s# 1-List of movies + Age requirement
+s# 2-Ehtraz if vaccinated y if not vaccinated print "sorry you cannot access the cenima"
+s# 3-List of movie showtime (each movie printed with it's showtime)
+g# 4-if age >13 not allowed without parent quardian if age 18=< print please choose seat
+s# 5-list = available seats, row a n/ row n/
+s# 6-print the seat chosen (seat A3) (it will be deleted from the og list)
+g# 7-available food (butter popcorn caramel popcorn, cheese popcorn, nachos, hotdog, fries, slush, water, coca cola, sprite )
+g# 8-do you want the food delivered to your seat or pickup.
+g# Thank you, enjoy your movie
+
+Movie names: The purge | Cruella | The Conjouring
+Seat list: A1-A5 | B1-B5 | C1-C5 | D1-D5
